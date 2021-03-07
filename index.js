52e8b86a884cf84cbd59e46d6557aa3afee3fbc7
Anup Agarwal <anupagarwal@Anups-MacBook-Air.local>
2021-03-07 21:48:08 +0530

    added port as env variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res, next) => {
   if (Math.ceil(Math.random() * 20) % 2)
